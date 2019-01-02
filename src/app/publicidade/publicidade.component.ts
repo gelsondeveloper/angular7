fca0cbc03a69dd47473f979424fcc464c23e49a9
gelsondeveloper <gelsonjupiter@gmail.com>
2019-01-02 18:08:14 +0100

    Publicidade com imagem dinâmica

--- src/app/publicidade/publicidade.component.ts
+++ src/app/publicidade/publicidade.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-publicidade',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicidade.component.scss']
 })
 export class PublicidadeComponent implements OnInit {
+  @Input() imagem: string;
 
   constructor() { }
 
